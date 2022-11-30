0c405e007c7c43876d33609a6114b8e4ed904453
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2022-11-30 23:43:12 +0200

    owl

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,65 @@
-const settingSlider = {
-    dots: false,
-    arrows: true,
-    infinite: true,
-    lazyLoad: 'ondemand',
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoPlay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    cssEase: 'linear',
-    mobileFirst: true,
-    responsive: [
-        {
-            breakpoints: 480,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoints: 760,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
-
-    ]
-}
-
-// $('.autoplay').slick({
+// const settingSlider = {
+//     dots: false,
+//     arrows: true,
+//     infinite: true,
+//     lazyLoad: 'ondemand',
+//     speed: 500,
 //     slidesToShow: 3,
 //     slidesToScroll: 1,
 //     autoplay: true,
+//     autoPlay: true,
 //     autoplaySpeed: 2000,
-// });
+//     fade: true,
+//     cssEase: 'linear',
+//     mobileFirst: true,
+//     responsive: [
+//         {
+//             breakpoints: 480,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 2,
+//             }
+//         },
+//         {
+//             breakpoints: 760,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//             }
+//         },
+//
+//     ]
+// }
+//
+// // $('.autoplay').slick({
+// //     slidesToShow: 3,
+// //     slidesToScroll: 1,
+// //     autoplay: true,
+// //     autoplaySpeed: 2000,
+// // });
+//
+//
+// $(document).ready(function (){
+//         $('.slider').slick(settingSlider);
+//     }
+// )
 
 
-$(document).ready(function (){
-        $('.slider').slick(settingSlider);
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: true,
+    autoplayTimeout:1500,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:3
+        }
     }
-)
-
-
-
-
+});
